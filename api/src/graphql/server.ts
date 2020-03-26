17ff19b4709354105fa7dfbc4ba97b4786ca9bd5
rudyyazdi <rudy@hireup.com.au>
2020-03-26 16:17:11 +1100

    Serve in memory bubbles

--- api/src/graphql/server.ts
+++ api/src/graphql/server.ts
@@ -1,15 +1,35 @@
 import { ApolloServer, gql } from 'apollo-server-express';
 
 const typeDefs = gql`
+  type Bubble {
+    title: String
+  }
   type Query {
-    hello: String
+    bubbles: [Bubble]
+  }
+  type Mutation {
+    addBubble(title: String): Bubble
   }
 `;
 
+interface Bubble {
+  title: string
+}
+
+const bubbles: Bubble[] = []
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    bubbles: () => bubbles,
   },
+  Mutation: {
+    addBubble: (root: any, args: Bubble, context: any) => {
+      bubbles.push(args)
+      console.log(root, args, context);
+
+      return args
+    }
+  }
 };
 
 export default new ApolloServer({ typeDefs, resolvers });
