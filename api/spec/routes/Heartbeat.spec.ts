5f4a93dd1d91f8019b5207e8631b298c00b3ea03
rudyyazdi <rudy@hireup.com.au>
2020-03-26 14:41:59 +1100

    type adjustment

--- api/spec/routes/Heartbeat.spec.ts
+++ api/spec/routes/Heartbeat.spec.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import Server from '@server';
 
 describe('heartbeat', () => {
-  it('should return 200 response code', done => {
+  it('should return 200 response code', (done: () => void) => {
     request(Server)
       .get('/heartbeat')
       .expect(200, '<3', done);
