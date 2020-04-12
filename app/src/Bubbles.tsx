510981f518a5910d3b749aecc2500e7a101043f6
rudyyazdi <rudy@hireup.com.au>
2020-04-12 10:00:26 +1000

    Get the extra input types

--- app/src/Bubbles.tsx
+++ app/src/Bubbles.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { loader } from 'graphql.macro';
 import { useQuery } from '@apollo/react-hooks';
 import {
@@ -8,7 +8,14 @@ import {
   Field,
 } from './GraphAst'
 import StackGrid from 'react-stack-grid';
-import { Button, Card, Elevation } from "@blueprintjs/core";
+import { 
+  Button,
+  Card,
+  Elevation,
+  Overlay,
+  FormGroup,
+  InputGroup,
+} from "@blueprintjs/core";
 
 import {DocumentNode} from 'graphql'
 
@@ -18,8 +25,16 @@ export interface IBubble {
   [key: string]: boolean | string | number | undefined
 }
 
+export interface IbubbleType {
+  fields: {name: string}[]
+}
+
+export interface IAddBubbleType {
+  args: {[key: string]: any}[]
+}
+
 const introspectionQuery = loader('./IntrospectionQuery.graphql')
-const selectBubbleType = ({__schema: {types}}: any) => types.filter((t: any) => t.name === 'Bubble')[0]
+const selectType = (type: string) => ({__schema: {types}}: any) => types.filter((t: any) => t.name === type)[0]
 
 const fieldNamesToQuery = (() => {
   let fieldNamesString: string;
@@ -48,14 +63,32 @@ const fieldNamesToQuery = (() => {
   }
 })()
 
-const AddBubbleCard = () => <Card elevation={Elevation.TWO} interactive>
+const AddBubbleCard = ({AddBubbleType}: {AddBubbleType: IAddBubbleType}) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const extraInputTypes = AddBubbleType.args.filter((t) => !['text', 'id'].includes(t.name))
+  console.log(extraInputTypes);
+  
+  return(<Card elevation={Elevation.TWO} interactive onClick={()=> setIsOpen(true)}>
+  <Overlay isOpen={isOpen} onClose={()=> setIsOpen(false)}> 
+    <div style={{textAlign: 'center',  width: '100%', marginTop: '100px', height: '0px'}}>
+      <div style={{width: '400px', margin: 'auto'}}>
+        <Card>
+          <FormGroup>
+            <InputGroup placeholder='text ...' />
+          </FormGroup>
+        </Card>
+      </div>
+    </div>
+  </Overlay>
   <p style={{textAlign: 'center', fontSize: '1.7em'}} >+</p>
-</Card>
+</Card>)
+}
+
 const Bubble = ({bubble}: {bubble: IBubble}) => <Card elevation={Elevation.TWO} interactive>
   <p>{bubble.text}</p>
 </Card>
 
-function BubbleGrid({bubbleType: {fields}}: {bubbleType: {fields: {name: string}[]}}): JSX.Element {
+function BubbleGrid({bubbleType: {fields}, AddBubbleType}: {bubbleType: IbubbleType, AddBubbleType: IAddBubbleType}): JSX.Element {
   const fieldNames = fields.map((f) => f.name)
   
   const { loading, error, data } = useQuery(fieldNamesToQuery(fieldNames), {pollInterval: 500});
@@ -65,8 +98,8 @@ function BubbleGrid({bubbleType: {fields}}: {bubbleType: {fields: {name: string}
   return(
     <div style={{marginTop: '15px'}}>
       <StackGrid columnWidth={350}>
-        <AddBubbleCard />
-        {data.searchBubbles.map((bubble: IBubble) => <Bubble bubble={bubble}/>)}
+        <AddBubbleCard AddBubbleType={AddBubbleType}/>
+        {data.searchBubbles.map((bubble: IBubble) => <Bubble bubble={bubble} key={bubble.id} />)}
       </StackGrid>
     </div>
   )
@@ -78,12 +111,17 @@ export const Bubbles = () => {
   
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
+
+    console.log(data);
+    
   
-  const bubbleType = selectBubbleType(data)
+  const bubbleType = selectType('Bubble')(data)
+  const MutationType = selectType('Mutation')(data)
+  const AddBubbleType = MutationType.fields.filter(({name}: any) => name === 'addBubble')[0]
 
   return (
     <div >
-      <BubbleGrid  bubbleType={bubbleType} />
+      <BubbleGrid  bubbleType={bubbleType} AddBubbleType={AddBubbleType} />
     </div>
   );
 }
